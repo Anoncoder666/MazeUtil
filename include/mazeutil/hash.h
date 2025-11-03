a16b8cc37d55d9f8e5bad3691ff9b183b049324e
Atharva <atharvasapre@gmail.com>
2025-11-02 22:36:05 -0500

    rmv

--- include/mazeutil/hash.h
+++ include/mazeutil/hash.h
@@ -7,7 +7,6 @@ struct pair_hash {
     std::size_t operator()(const std::pair<T1,T2>& p) const noexcept {
         auto h1 = std::hash<T1>{}(p.first);
         auto h2 = std::hash<T2>{}(p.second);
-        // Simple but effective hash combine
         return h1 ^ (h2 << 1);
     }
 };
